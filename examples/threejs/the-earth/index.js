3e19f33812b8302194f960bddf1a9d3414b79e23
vudinhthu <vudinhthu87@gmail.com>
2026-05-08 11:44:25 +0700

    feat: migrate comimgsoon template files and initialize new triship template structure

--- examples/threejs/the-earth/index.js
+++ examples/threejs/the-earth/index.js
@@ -4,11 +4,13 @@ import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 import getStarfield from "./src/getStarfield.js";
 import { getFresnelMat } from "./src/getFresnelMat.js";
 
+const w = window.innerWidth;
+const h = window.innerHeight;
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
 camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 // THREE.ColorManagement.enabled = true;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
@@ -28,7 +30,7 @@ const material = new THREE.MeshPhongMaterial({
 	bumpScale: 0.04,
 })
 const earthMesh = new THREE.Mesh(geometry,material);
-earthGroup.add(earthMesh);
+scene.add(earthMesh);
 
 const lightsMat = new THREE.MeshBasicMaterial({
 	map: loader.load("./textures/03_earthlights1k.jpg"),
@@ -61,20 +63,8 @@ const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
 sunLight.position.set(-2, 0.5, 1.5);
 scene.add(sunLight);
 
-
-
-function handleWindowResize() {
-	let _w = window.innerWidth;
-    let _h = window.innerHeight;
-	camera.aspect = _w / _h;
-	camera.updateProjectionMatrix();
-	renderer.setSize(_w, _h);
-    renderer.setViewport(0,0,_w,_h);
-}
-const resizeObserver = new ResizeObserver(handleWindowResize);
-resizeObserver.observe(document.body);
-
 function animate() {
+	requestAnimationFrame(animate);
 	earthMesh.rotation.y += 0.002;
 	lightsMesh.rotation.y += 0.002;
 	cloudsMesh.rotation.y += 0.0023;
@@ -82,4 +72,12 @@ function animate() {
 	stars.rotation.y -= 0.0002;
 	renderer.render(scene, camera);
 }
-renderer.setAnimationLoop(animate);
+
+animate();
+
+function handleWindowResize() {
+	camera.aspect = w / h;
+	camera.updateProjectionMatrix();
+	renderer.setSize(w, h);
+}
+window.addEventListener('resize', handleWindowResize, false);
